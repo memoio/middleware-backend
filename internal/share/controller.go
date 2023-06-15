4927f2e5e89ec0a157c93f2a1aefc1b4810336e4
dzh <1051072440@qq.com>
2023-06-15 14:53:04 +0800

    add share

---
+++ internal/share/controller.go
@@ -0,0 +1,107 @@
+package share
+
+import (
+	"time"
+
+	"github.com/memoio/backend/internal/database"
+	"github.com/memoio/backend/internal/storage"
+	"golang.org/x/xerrors"
+)
+
+type CreateShareRequest struct {
+	MID             string              `josn:"mid"`
+	SType           storage.StorageType `json:"type"`
+	Password        string              `json:"password"`
+	ExpiredTime     int64               `josn:"expire"`
+	RemainDownloads int                 `json:"downloads"`
+}
+
+func CreateShare(address string, chainID int, request CreateShareRequest) (string, error) {
+	// address := c.GetString("address")
+	// chainID := c.GetInt("chainid")
+
+	// 查看文件是否存在，且属于该用户
+	fileInfo, err := database.Get(chainID, address, request.MID, request.SType)
+	if err != nil {
+		return "", xerrors.Errorf("Can't find the file")
+	}
+
+	newShare := ShareObjectInfo{
+		UserID:          Identity{address, chainID},
+		MID:             request.MID,
+		SType:           request.SType,
+		FileName:        fileInfo.Name,
+		Password:        request.Password,
+		ExpiredTime:     time.Now().Add(time.Duration(request.ExpiredTime) * time.Second).Unix(),
+		RemainDownloads: request.RemainDownloads,
+	}
+
+	id, err := newShare.CreateShare()
+	if err != nil {
+		return "", xerrors.Errorf("Failed to create share link")
+	}
+
+	// baseUrl := GetBaseUrl()
+	// return baseUrl.String() + "/s/" + id, nil
+	return "http://localhost:8081/s/" + id, nil
+}
+
+type UpdateShareRequest struct {
+	Attribute string `json:"attribute"`
+	Value     string `json:"value"`
+}
+
+func UpdateShare(share *ShareObjectInfo, request UpdateShareRequest) error {
+	return share.UpdateShare(request.Attribute, request.Value)
+}
+
+func DeleteShare(address string, chainID int, shareID string) error {
+	share := GetShareByID(shareID)
+	if share == nil {
+		return xerrors.Errorf("share link not found")
+	}
+	if share.UserID.Address != address || share.UserID.ChainID != chainID {
+		return xerrors.Errorf("there is not your share link, can't delete")
+	}
+
+	return share.DeleteShare()
+}
+
+// func ListShares()
+
+type GetShareRequest struct {
+	Password string `json:"password"`
+}
+
+func GetShare(address string, chainID int, share *ShareObjectInfo, request GetShareRequest) (*ShareObjectInfo, error) {
+	var unlocked = true
+	if share.Password != "" {
+		_, unlocked = MemoCache.Load("unlock" + address + share.ShareID)
+		// 当前用户未输入相应的密码解锁
+		if !unlocked {
+			if share.Password == request.Password {
+				unlocked = true
+				MemoCache.Store("unlock"+address+share.ShareID, struct{}{})
+			}
+		}
+	}
+
+	if !unlocked {
+		return nil, xerrors.Errorf("Please enter the correct password")
+	}
+
+	return share, nil
+}
+
+func SaveShare(address string, chainID int, share *ShareObjectInfo) error {
+	info, err := database.Get(chainID, address, share.MID, share.SType)
+	if err != nil {
+		return err
+	}
+
+	info.Address = address
+	info.ChainID = chainID
+
+	_, err = database.Put(info)
+	return err
+}
