387fcd432c3ad4c9f5e48ec5f8c7f03b71388bc9
dzh <1051072440@qq.com>
2024-01-30 16:05:54 +0800

    Add: support bitcoin taproot address

---
+++ internal/authentication/btcaddress.go
@@ -0,0 +1,95 @@
+package auth
+
+import (
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+)
+
+func getP2PKHAddress(publicKey *btcec.PublicKey) string {
+	address, err := btcutil.NewAddressPubKey(publicKey.SerializeCompressed(), &chaincfg.MainNetParams)
+	if err != nil {
+		return ""
+	}
+
+	return address.EncodeAddress()
+}
+
+func getP2SHAddress(publicKey *btcec.PublicKey) string {
+	witnessProg := btcutil.Hash160(publicKey.SerializeCompressed())
+	addressWitnessPubKeyHash, err := btcutil.NewAddressWitnessPubKeyHash(witnessProg, &chaincfg.MainNetParams)
+	if err != nil {
+		return ""
+	}
+
+	serializedScript, err := txscript.PayToAddrScript(addressWitnessPubKeyHash)
+	if err != nil {
+		return ""
+	}
+	addressScriptHash, err := btcutil.NewAddressScriptHash(serializedScript, &chaincfg.MainNetParams)
+	if err != nil {
+		return ""
+	}
+
+	return addressScriptHash.EncodeAddress()
+}
+
+func getNativeSegWitAddress(publicKey *btcec.PublicKey) string {
+	witnessProg := btcutil.Hash160(publicKey.SerializeCompressed())
+	addressWitnessPubKeyHash, err := btcutil.NewAddressWitnessPubKeyHash(witnessProg, &chaincfg.MainNetParams)
+	if err != nil {
+		return ""
+	}
+	return addressWitnessPubKeyHash.EncodeAddress()
+}
+
+func getTaprootAddress(publicKey *btcec.PublicKey) string {
+	tapKey := computeTaprootOutputKey(publicKey, []byte{})
+	address, err := btcutil.NewAddressTaproot(schnorr.SerializePubKey(tapKey), &chaincfg.MainNetParams)
+	if err != nil {
+		return ""
+	}
+	return address.EncodeAddress()
+}
+
+func computeTaprootOutputKey(pubKey *btcec.PublicKey,
+	scriptRoot []byte) *btcec.PublicKey {
+
+	// This routine only operates on x-only public keys where the public
+	// key always has an even y coordinate, so we'll re-parse it as such.
+	internalKey, _ := schnorr.ParsePubKey(schnorr.SerializePubKey(pubKey))
+
+	// First, we'll compute the tap tweak hash that commits to the internal
+	// key and the merkle script root.
+	tapTweakHash := chainhash.TaggedHash(
+		chainhash.TagTapTweak, schnorr.SerializePubKey(internalKey),
+		scriptRoot,
+	)
+
+	// With the tap tweek computed,  we'll need to convert the merkle root
+	// into something in the domain we can manipulate: a scalar value mod
+	// N.
+	var tweakScalar btcec.ModNScalar
+	tweakScalar.SetBytes((*[32]byte)(tapTweakHash))
+
+	// Next, we'll need to convert the internal key to jacobian coordinates
+	// as the routines we need only operate on this type.
+	var internalPoint btcec.JacobianPoint
+	internalKey.AsJacobian(&internalPoint)
+
+	// With our intermediate data obtained, we'll now compute:
+	//
+	// taprootKey = internalPoint + (tapTweak*G).
+	var tPoint, taprootKey btcec.JacobianPoint
+	btcec.ScalarBaseMultNonConst(&tweakScalar, &tPoint)
+	btcec.AddNonConst(&internalPoint, &tPoint, &taprootKey)
+
+	// Finally, we'll convert the key back to affine coordinates so we can
+	// return the format of public key we usually use.
+	taprootKey.ToAffine()
+
+	return btcec.NewPublicKey(&taprootKey.X, &taprootKey.Y)
+}
